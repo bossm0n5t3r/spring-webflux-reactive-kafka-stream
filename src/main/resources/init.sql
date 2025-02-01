9f7c8682eb57aa151988fd6b1b7f2351b1e60794
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-02-02 04:24:00 +0900

    feat: Update init.sql

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS stock_prices (
-	symbol String,
-	original_price Float64,
-	currency String,
-	timestamp DateTime64(9, 'UTC')
+    symbol String,
+    original_price Float64,
+    currency String,
+    timestamp DateTime64(9, 'UTC')
 ) ENGINE = MergeTree() ORDER BY (symbol, timestamp);
 
 CREATE TABLE IF NOT EXISTS avg_stock_prices (
@@ -19,7 +19,7 @@ CREATE MATERIALIZED VIEW avg_stock_prices_mv TO avg_stock_prices AS
 SELECT
     toStartOfMinute(timestamp) AS date_time,
     symbol,
-	currency,
+    currency,
     avgState(original_price) AS avg_price
 FROM stock_prices
 GROUP BY symbol, currency, date_time;
